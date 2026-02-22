ff9ab5737998e97bd146a3dc4953b188c1e41727
Mater <88980654+Mater8600@users.noreply.github.com>
2026-02-21 22:31:46 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+import random
+try:
+    import scapy 
+    import pychromecast 
+    import typer
+    import rich
+    import pylaunch
+except:
+    print("Installing your dependancies.")
+    os.popen("sudo pip install scapy pychromecast flask pylaunch rich typer  --break-system-packages")
+    print("Finished installing all the requirments.")
+
+### Starting the pwnapple hotspot ###
+while True:
+    choice = input("Would you like to enable the pwnapple hotspot?\nY or N?\n")
+    if choice.upper() == "Y" or choice.upper() == "YES":
+        print("Setting up pwnapple hotspot\n")
+        ssid = input("Please input the ssid/name of the network:\n")
+        characters = ['a','b', 'c', 'd', 'e','f','d','e','f','g','h','i','j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z',
+                    '1','2','3','4','5','6','7','8','9','0']
+        password = ""
+        print("Creating password...")
+        for x in range(0,8):
+            password += random.choice(characters)
+        print(f"Your password is: {password}\n")
+        device = input("Please input your wifi card for the creation of the hotspot:\n(type 0 for wlan0)\n")
+        if device == "0":
+            os.popen(f"sudo nmcli device wifi hotspot ssid {ssid} password {password} ifname wlan0")
+        else:
+             os.popen(f"sudo nmcli device wifi hotspot ssid {ssid} password {password} ifname {device}")
+    elif choice.upper() == "N" or choice.upper() == "NO":
+        print("Ok,Skipping the hotspot setup")
+        break
+    else:
+        print("Not a vaild option\n")
+print("Thank you for setting up pwnapple, hope you enjoy!")
