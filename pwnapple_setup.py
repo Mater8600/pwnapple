2a139f2a5e747fd05322ff38ac43816eeb97201b
Mater <88980654+Mater8600@users.noreply.github.com>
2026-02-21 22:41:17 -0400

    Enhance hotspot setup with QR code display
    Added QR code generation for easy connection to the hotspot.

--- pwnapple_setup.py
+++ pwnapple_setup.py
@@ -10,7 +10,7 @@ except:
     print("Installing your dependancies.")
     os.popen("sudo pip install scapy pychromecast flask pylaunch rich typer  --break-system-packages")
     print("Finished installing all the requirments.")
-
+os.popen("sudo apt install qrencode ")
 ### Starting the pwnapple hotspot ###
 while True:
     choice = input("Would you like to enable the pwnapple hotspot?\nY or N?\n")
@@ -27,11 +27,16 @@ while True:
         device = input("Please input your wifi card for the creation of the hotspot:\n(type 0 for wlan0)\n")
         if device == "0":
             os.popen(f"sudo nmcli device wifi hotspot ssid {ssid} password {password} ifname wlan0")
+            print("Here's a qr code for a easy connection :)")
+            os.popen(f"nmcli wlan0 wifi show-password")
         else:
              os.popen(f"sudo nmcli device wifi hotspot ssid {ssid} password {password} ifname {device}")
+             print("Here's a qr code for a easy connection :)")
+             os.popen(f"nmcli {device} wifi show-password")
     elif choice.upper() == "N" or choice.upper() == "NO":
         print("Ok,Skipping the hotspot setup")
         break
     else:
         print("Not a vaild option\n")
+
 print("Thank you for setting up pwnapple, hope you enjoy!")
